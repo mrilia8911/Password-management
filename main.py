f0a4e73fbd892ee4f32d89cea0884d55030e3923
Seyed Saeed Mohammadi <49374230+saeed090@users.noreply.github.com>
2024-08-23 10:11:21 +0330

    Create main.py
    this is first

---
+++ main.py
@@ -0,0 +1,22 @@
+import random
+import string
+
+lower = string.ascii_lowercase
+uppper = string.ascii_uppercase
+print(uppper)
+print(string.ascii_letters)
+symbols = "!@#$%^&*()-+[]{}"
+numbers = "0123456789"
+all = lower + uppper + symbols + numbers
+
+while True:
+    print("choose an option :\n\t1) create a password\n\t2) exit")
+    choise = input("your choice : ")
+    if choise == "1":
+        length = int(input("enter the password length : "))
+        password = "".join(random.sample(all, length))
+        print(password)
+    elif choise == "2":
+        break
+    else:
+        print("invalid option")
